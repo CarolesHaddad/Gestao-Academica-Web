534e0d4e2847139f8c182f720d94dd22ccce424b
Dimitri <dimitrilouback91@gmail.com>
2024-03-10 01:38:46 -0300

    gets e sets

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
         Disciplina disciplina1 = new Disciplina("matematica", 60);
         Nota nota1 = new Nota(10,10,disciplina1);
         Aluno aluno1 = new Aluno(1, "Dimitri", 20, "202111230", disciplina1);
 
         System.out.println(aluno1.getDisciplina());
+
+
 
     }
 }
